7bf5e30747ece391d84740e8a48e0be469d2a964
Khobe Arzadon <khobearzadon@yahoo.com>
2024-07-16 13:21:35 -0700

    changes

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,18 +2,30 @@ import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ProtectedRoute({ children }) {
   const [isAuthorized, setIsAuthorized] = useState(null);
 
+  useEffect(() => {
+    auth().catch(() => setIsAuthorized(false));
+  }, []);
+
   const refreshToken = async () => {
     const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-      try {
-        const res = await
+    try {
+      const res = await api.post("/api/token/refresh/", {
+        refresh: refreshToken,
+      });
+      if (res.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+      }
     } catch (error) {
-        console.log(error)
-        setIsAuthorized(false)
+      console.log(error);
+      setIsAuthorized(false);
     }
   };
 
